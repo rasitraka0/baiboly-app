a4d4754178774ab3609a3399b3d9f17780ff51d0
rasitraka0 <sitrakaheritinaandriambarijao@gmail.com>
2026-02-24 22:58:06 +0300

    Ajout de la route GET /recherche

--- backend/routes/bible.js
+++ backend/routes/bible.js
@@ -24,4 +24,17 @@ router.get('/livres/:livreId/chapitres/:chapitre', async (req, res) => {
   }
 });
 
+router.get('/recherche', async (req, res) => {
+  const motRecherche = req.query.q;
+  try {
+    const resultat = await pool.query(
+      'SELECT * FROM versets WHERE texte ILIKE $1',
+      [`%${motRecherche}%`],
+    );
+    res.json(resultat.rows);
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
+});
+
 module.exports = router;
