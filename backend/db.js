07c94ee371c5334a771583ad42c37575ad31d425
rasitraka0 <sitrakaheritinaandriambarijao@gmail.com>
2026-03-05 10:31:57 +0300

    amelioration de db.js pour render et localement

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      },
+);
 
 module.exports = pool;
